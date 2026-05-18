6a09999ca742d921fb9bcc3e61634216999032f3
Hugo Vera <hugomvera@gmail.com>
2026-05-18 11:35:29 -0700

    The create update and delete notifications are working plus tap action

--- app/src/main/java/com/example/demonotification/MainActivity.kt
+++ app/src/main/java/com/example/demonotification/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.demonotification
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -66,7 +68,9 @@ private fun createNotificationChannel(context: Context) {
     }
 }
 
-
+//this is where the button will be created
+//TODO add a button that will delte the notification
+//TODO add a button that will update the notification
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
@@ -80,15 +84,128 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     ) {
         Text(text = "Hello $name!", modifier = modifier)
 
+
+
         Button(onClick = {
   showNotification(context)
         }) {
             Text("Show Notification")
         }
+
+
+        Button(onClick = {
+            updateNotification(context)
+        }) {
+            Text("Update Notification")
+        }
+
+
+        Button(onClick = {
+            deleteNotification(context)
+        }) {
+            Text("Delete Notification")
+        }
+
+
     }
+
+
+
+
 }
 
 fun showNotification(context: Context) {
+
+// Create an explicit intent for an Activity in your app.
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    val pendingIntent: PendingIntent =
+        PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+
+    val CHANNEL_ID = "my_channel_id"
+
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.notification_icon)
+        .setContentTitle("My notification")
+        .setContentText("Much longer text that cannot fit one line...")
+        .setStyle(NotificationCompat.BigTextStyle()
+            .bigText("Much longer text that cannot fit one line..."))
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        // Set the intent that fires when the user taps the notification.
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
+
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling ActivityCompat#requestPermissions here
+            // to request the missing permissions, and then overriding
+            // public fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+            //                                        grantResults: IntArray)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return@with
+        }
+        // notificationId is a unique int for each notification that you must define.
+        notify(101, builder.build())
+    }
+
+
+}
+
+
+
+
+//this is updateNotification
+fun updateNotification(context: Context) {
+
+
+
+    val CHANNEL_ID = "my_channel_id"
+
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.notification_icon)
+        .setContentTitle("Updated  notification is updated ")
+        .setContentText("Updated Much longer text that cannot fit one line...")
+        .setStyle(NotificationCompat.BigTextStyle()
+            .bigText(" Updated Much longer text that cannot fit one line..."))
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        //set autocancel to true so that the notification is removed after clicking on it
+        .setAutoCancel(true)
+
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling ActivityCompat#requestPermissions here
+            // to request the missing permissions, and then overriding
+            // public fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+            //                                        grantResults: IntArray)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return@with
+        }
+        // notificationId is a unique int for each notification that you must define.
+        notify(101, builder.build())
+    }
+
+
+}
+
+
+
+
+fun deleteNotification(context: Context) {
 
     val CHANNEL_ID = "my_channel_id"
 
@@ -116,11 +233,13 @@ fun showNotification(context: Context) {
             return@with
         }
         // notificationId is a unique int for each notification that you must define.
-        notify(101, builder.build())
+       // notify(101, builder.build())
+        cancel(101)
     }
 
 
 }
+
 
 
 @Preview(showBackground = true)
